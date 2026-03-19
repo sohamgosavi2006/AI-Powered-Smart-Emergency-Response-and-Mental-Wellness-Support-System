8d5d2c881427fcca4812c7a08d485df790de6c2e
Soham Gosavi <92179040+sohamgosavi2006@users.noreply.github.com>
2026-03-19 23:40:50 +0530

    updated web app

--- AI Web App/ai-project/test-models.mjs
+++ AI Web App/ai-project/test-models.mjs
@@ -1,4 +1,4 @@
-const apiKey = "AIzaSyDZr_AAo_2SjtxGQUzUNzliBXjDIG6eAbM";
+const apiKey = "";
 async function run() {
   try {
     const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`);
